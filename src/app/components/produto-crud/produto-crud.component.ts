c61a7a0919731e9b14a561e286ca1e6f36d9a796
Mateus Vitor <43605106+MaVitor@users.noreply.github.com>
2025-07-03 20:14:01 +0000

    feat: Atualiza a versao do angular pra 19

--- src/app/components/produto-crud/produto-crud.component.ts
+++ src/app/components/produto-crud/produto-crud.component.ts
@@ -7,6 +7,7 @@ import { ProdutoService } from "../../services/produto.service"
   selector: "app-produto-crud",
   templateUrl: "./produto-crud.component.html",
   styleUrls: ["./produto-crud.component.scss"],
+  standalone: false,
 })
 export class ProdutoCrudComponent implements OnInit {
   produtos: Produto[] = []
